e73975699d3f34cb561f82b120cc8e73724050ad
Edulamin <edulamin10@gmail.com>
2025-05-11 23:47:58 -0300

    Core requirements Part

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,9 +1,51 @@
 using System;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("Put here your grade percentage: ");
+        string userInput = Console.ReadLine();
+        int gradePercentage = int.Parse(userInput);
+
+        string letter = "";
+
+        if (gradePercentage >= 90)
+        {
+            letter = "A";
+        }
+
+        else if (gradePercentage >= 80 && gradePercentage < 90)
+        {
+            letter = "B";
+        }
+
+        else if (gradePercentage >= 70 && gradePercentage < 80)
+        {
+            letter = "C";
+        }
+
+        else if(gradePercentage >= 60 && gradePercentage < 70)
+        {
+            letter = "D";
+        }
+
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your percentage corresponds to the letter {letter}");
+
+        if (gradePercentage >= 70)
+        {
+            Console.WriteLine("Congratulations, you've been approved!");
+        }
+        else
+        {
+            Console.WriteLine("Sorry, you didn't pass this time. Try doing it again!");
+        }
     }
 }
