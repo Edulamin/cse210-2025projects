289f962da6c9d7e085ba5cdbafb6717183fb2672
Edulamin <edulamin10@gmail.com>
2025-05-13 00:35:35 -0300

    Adding the finished program.

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        DisplayWelcome();
+
+        string name = PromptUserName();
+        int number = PromptUserNumber();
+        int square = SquareNumber(number);
+
+        DisplayResult(name, square);
+
     }
+        static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the program! :)");
+        }
+
+        static string PromptUserName()
+        {
+            Console.Write("What is your name? ");
+            string userName = Console.ReadLine();
+            
+            return userName;
+        }
+
+        static int PromptUserNumber()
+        {
+            Console.Write("What is your favorite number? ");
+            int userNumber = int.Parse(Console.ReadLine());
+
+            return userNumber;
+        }
+
+        static int SquareNumber(int number)
+        {
+            int squaredNumber = number * number;
+
+            return squaredNumber;
+        }
+
+        static void DisplayResult(string name, int square)
+        {
+            Console.Write($"{name}, the square of your number is {square}");
+        }
+
 }
